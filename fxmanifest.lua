2084cf759de945389cc15ec7def7b904bf520ffc
WALKERIS <123303978+WALKERIS@users.noreply.github.com>
2024-04-04 21:29:49 +0300

    release 1.0
    requirements {
    ox_lib,
    es_extended
    }

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+shared_script '@FGM/ai_module_fg-obfuscated.lua'
+shared_script '@FGM/shared_fg-obfuscated.lua'
+fx_version 'bodacious'
+lua54 'yes' 
+game 'gta5'
+author 'WALKER'
+discription 'TEST PROJECT MADE FOR VISION RP| BY WALKER| NO UPDATES PROMISED'
+version '1.0'
+
+client_script "client/client.lua"
+
+
+
+server_script {
+            "server/server.lua"
+}
+
+
+shared_scripts {
+    '@ox_lib/init.lua',
+}

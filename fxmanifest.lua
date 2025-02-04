f2ccfd311a32de27aa97a54c4207375dca2f9ca7
WALKERIS <123303978+WALKERIS@users.noreply.github.com>
2025-02-04 21:39:13 +0200

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,28 +1,22 @@
+server_script '@ElectronAC/src/include/server.lua'
+client_script '@ElectronAC/src/include/client.lua'
 fx_version 'bodacious'
 lua54 'yes' 
 game 'gta5'
 author 'WALKER'
 discription 'TEST PROJECT MADE FOR VISION RP| BY WALKER| NO UPDATES PROMISED'
 version '1.0'
-
 client_script {
     "config.lua",
     "client/client.lua"
 }
-
-
-
 server_script {
     "config.lua",
     "server/server.lua"
 }
-
-
 shared_scripts {
     '@ox_lib/init.lua',
 }
-
 escrow_ignore {
     'config/config.lua',
-
 }
